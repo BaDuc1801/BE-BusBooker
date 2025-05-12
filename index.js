00d6d3571564c0be47a68692527c7fe16047020e
unknown <minhduc180104@gmail.com>
2025-05-12 17:12:47 +0700

    add refresh_token

--- index.js
+++ index.js
@@ -8,16 +8,15 @@ import scheduleRouter from './routes/scheduleRouter.js';
 import voucherRouter from './routes/voucherRouter.js';
 import ticketRouter from './routes/ticketRouter.js';
 import notiRouter from './routes/notiRouter.js';
-import emailRouter from './routes/emailRouter.js';
 import dotenv from "dotenv";
 dotenv.config();
 
 await mongoose.connect(process.env.MONGOCONNECT)
 
 const corsOptions = {
-    origin: 'http://localhost:3000',  
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
-    credentials: true, 
+    origin: ['http://localhost:3000', 'https://be-bus-booker.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
 };
 
 const app = express();
@@ -31,7 +30,6 @@ app.use('/schedule', scheduleRouter)
 app.use('/vouchers', voucherRouter)
 app.use('/tickets', ticketRouter)
 app.use('/noti', notiRouter)
-app.use('/email', emailRouter)
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "hello" });
