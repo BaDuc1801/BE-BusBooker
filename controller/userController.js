567d698f1784bf0a9147efa660c719d19ea35846
unknown <minhduc180104@gmail.com>
2025-05-15 15:59:33 +0700

    test api

--- controller/userController.js
+++ controller/userController.js
@@ -42,7 +42,7 @@ const userController = {
             const accessToken = jwt.sign({
                 userId: user._id,
                 role: user.role
-            }, process.env.SECRETKEY, { expiresIn: "1h" });
+            }, process.env.SECRETKEY, { expiresIn: "5m" });
 
             const refreshToken = jwt.sign({
                 userId: user._id,
